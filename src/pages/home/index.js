e72f1c1e18fbf8592af2f3c10d55104acac747ca
Diego Carciente <diego.carciente@gmail.com>
2020-07-20 14:04:54 -0400

    feature: estructura de servicios

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,29 +7,32 @@ import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 
 import './style.css'
-import serviceGetAccounts from './services/get-accounts'
+import serviceGetAccounts from './services'
 
 class Home extends React.Component {
 	constructor(props) {
 		super(props)
 		console.log('1. constructor')
-
 		this.state = {
 			title: 'Hola Mundo!',
 			service: null,
+			accounts: {}
 		}
 	}
 
-	handleServiceGetAccounts = async (item) => {
-		let accounts = await serviceGetAccounts()
-		console.log(accounts);
-		this.setState({ service: accounts.itemsPerPage })
+	handleFormatNumber(){
+
 	}
 
-
-	componentDidMount() {
+	async componentDidMount () {
 		console.log('3. componentDidMount')
-		this.handleServiceGetAccounts()
+		let accounts = await serviceGetAccounts()
+		console.log(accounts);
+		
+		this.setState({ 
+			service: accounts.itemsPerPage,
+			accounts: accounts.data
+		})
 	}
 
 	render() {
@@ -43,6 +46,7 @@ class Home extends React.Component {
 					<DatePicker placeholder='select date' />
 				</>
 				<p> Servicio nuevo: { this.state.service }</p>
+				<p>Objeto { JSON.stringify(this.state.accounts) }</p>
 				<Footer />
 			</div>
 		)
