dc32844a119fd8327cb55640ff701fa376d427bd
Carlos Espinoza <carlosarmikhael@gmail.com>
2020-07-23 00:37:27 -0400

    sintaxis de componente account

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -6,23 +6,24 @@ import { Link } from 'react-router-dom'
 import './style.css'
 
 class Account extends React.Component {
-
 	render() {
-        console.log(this.props.items);
-        
 		if (this.props.items.length >= 0) {
 			return (
 				<div>
-					{
-						this.props.items.map(function(item, i){
-                        return <Link key={i} to={`/account/${item._id}`}><li>{item.name} - {item.email}</li></Link> 
-						})
-					}
+					{this.props.items.map(function (item, i) {
+						return (
+							<Link key={i} to={`/account/${item._id}`}>
+								<li>
+									{item.name} - {item.email}
+								</li>
+							</Link>
+						)
+					})}
 				</div>
 			)
 		} else {
-            return 'Cargando...'
-        }
+			return 'Cargando...'
+		}
 	}
 }
 
