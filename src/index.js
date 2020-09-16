5aac3a7ec362f2af79f9bad809c8d91c1d137780
Carlos Espinoza <carlosarmikhael@gmail.com>
2020-09-15 21:41:05 -0300

    cambios esteticos

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 import 'antd/dist/antd.css'
 
+import './app.css'
 import Routers from './routers/Routers'
 
 ReactDOM.render(<Routers />, document.getElementById('root'))
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,9 @@
 /** @format */
 
 import React from 'react'
-import { Button, DatePicker } from 'antd'
+import Masonry from 'react-masonry-css'
+
+import { Button, DatePicker, Layout } from 'antd'
 
 import Footer from '../../components/Footer/Footer'
 import Loading from '../../components/Loading/Loading'
@@ -9,6 +11,8 @@ import Account from '../../components/Account/Account'
 
 import './style.css'
 import serviceGetAccounts from './services'
+
+const { Content } = Layout
 
 class Home extends React.Component {
 	constructor(props) {
@@ -38,6 +42,37 @@ class Home extends React.Component {
 		}
 		return (
 			<React.Fragment>
+				<Content className='cv-container-main'>
+					<Masonry
+						breakpointCols={{ default: 4, 720: 2 }}
+						className='my-masonry-grid'
+						columnClassName='my-masonry-grid_column'>
+						<div style={{ height: '100px' }}>My Element</div>
+						<div style={{ height: '150px' }}>My Element</div>
+						<div style={{ height: '250px' }}>My Element</div>
+						<div style={{ height: '150px' }}>My Element</div>
+						<div style={{ height: '100px' }}>My Element</div>
+						<div style={{ height: '70px' }}>My Element</div>
+						<div style={{ height: '170px' }}>My Element</div>
+						<div style={{ height: '200px' }}>My Element</div>
+						<div style={{ height: '100px' }}>My Element</div>
+						<div style={{ height: '150px' }}>My Element</div>
+						<div style={{ height: '250px' }}>My Element</div>
+						<div style={{ height: '150px' }}>My Element</div>
+						<div style={{ height: '100px' }}>My Element</div>
+						<div style={{ height: '70px' }}>My Element</div>
+						<div style={{ height: '170px' }}>My Element</div>
+						<div style={{ height: '200px' }}>My Element</div>
+						<div style={{ height: '100px' }}>My Element</div>
+						<div style={{ height: '150px' }}>My Element</div>
+						<div style={{ height: '250px' }}>My Element</div>
+						<div style={{ height: '150px' }}>My Element</div>
+						<div style={{ height: '100px' }}>My Element</div>
+						<div style={{ height: '70px' }}>My Element</div>
+						<div style={{ height: '170px' }}>My Element</div>
+						<div style={{ height: '200px' }}>My Element</div>
+					</Masonry>
+				</Content>
 				<h1>{this.state.title}</h1>
 				<p>{process.env.REACT_APP_ENVIROMENT}</p>
 				<>
