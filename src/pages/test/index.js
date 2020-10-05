02d8616acb07c669e7c68cae320a139fe79be158
Diego Carciente <diego.carciente@gmail.com>
2020-10-05 10:33:32 -0300

    integracion con google login

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,8 +1,9 @@
 /** @format */
 
 import React from 'react'
-import InfiniteScroll from 'react-infinite-scroll-component'
 import axios from 'axios'
+import FacebookLogin from 'react-facebook-login';
+import { GoogleLogin } from 'react-google-login';
 
 class Test extends React.Component {
 	state = {
@@ -10,43 +11,79 @@ class Test extends React.Component {
 		pageNumber: 1,
 		items: 15,
 		hasMore: true,
+		object: {}
 	}
 
 	componentDidMount() {
 		//initial request is sent
-		this.handleList()
+		this.testInit()
 	}
 
-	handleList = () => {
-		axios
-			.get(
-				`https://jsonplaceholder.typicode.com/photos?_page=${this.state.pageNumber}&_limit=15`
-			)
-			.then((res) =>
-				this.setState({
-					//updating data
-					list: [...this.state.list, ...res.data],
-					//updating page numbers
-					pageNumber: this.state.pageNumber + 1,
-				})
-			)
+	testInit = async () => {
+		const { data } = await axios.get('https://www.instagram.com/jpixelat')
+		const jsonObject = data.match(/<script type="text\/javascript">window\._sharedData = (.*)<\/script>/)[1].slice(0, -1)
+		const jsonParse = JSON.parse(jsonObject);
+		const userInfo = jsonParse.entry_data.ProfilePage[0].graphql.user
+		this.setState({
+			object: userInfo,
+			biography: userInfo.biography,
+			followers: userInfo.edge_followed_by.count,
+			followed: userInfo.edge_follow.count,
+			post: userInfo.edge_owner_to_timeline_media.count,
+		})
+		console.log(userInfo);
 	}
 
 	render() {
+
+		var finished_rendering = function() {
+			console.log("finished rendering plugins");
+			var spinner = document.getElementById("spinner");
+			spinner.removeAttribute("style");
+			spinner.removeChild(spinner.childNodes[0]);
+		}
+		const responseFacebook = (response) => {
+			console.log(response);
+		}
+
+		const responseGoogle = (response) => {
+			console.log(response);
+		  }
 		return (
 			<div className='App'>
-				<InfiniteScroll
-					dataLength={this.state.list.length} //This is important field to render the next data
-					next={this.handleList}
-					hasMore={this.state.hasMore}
-					loader={<h4>Loading...</h4>}>
-					{this.state.list.map((item) => (
-						<ul className='user' key={item.title}>
-							<li>Name: {item.title}</li>
-							<img alt='hola' title='holaa' src={item.thumbnailUrl} />
-						</ul>
-					))}
-				</InfiniteScroll>
+				<p>Biografia: {this.state.biography}</p>
+				<p>Seguidores: {this.state.followers}</p>
+				<p>Seguidos: {this.state.followed}</p>
+				<p>Publicaciones: {this.state.post}</p>
+				<hr></hr>
+				{/* <FacebookLogin
+					appId="2737781739844102"
+					autoLoad
+					callback={responseFacebook}
+					render={renderProps => (
+						<button onClick={renderProps.onClick}>This is my custom FB button</button>
+					)}
+				/>
+
+				<div
+					class="fb-login-button"
+					data-max-rows="1"
+					data-size="large"
+					data-button-type="continue_with"
+					data-use-continue-as="true"
+				></div> */}
+
+				<hr></hr>
+				<GoogleLogin
+					clientId="264087860616-jckkkgv633q5r4n2othpppgk6rarhf03.apps.googleusercontent.com"
+					render={renderProps => (
+						<button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
+					)}
+					buttonText="Login"
+					onSuccess={responseGoogle}
+					onFailure={responseGoogle}
+					cookiePolicy={'single_host_origin'}
+				/>
 			</div>
 		)
 	}
