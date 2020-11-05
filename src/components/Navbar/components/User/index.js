7e520c59e99fcbdae13a2fae5f03757cbfdd9399
Carlos Espinoza <carlosarmikhael@gmail.com>
2020-11-04 22:23:28 -0300

    perfil y crear cuenta

--- src/components/Navbar/components/User/index.js
+++ src/components/Navbar/components/User/index.js
@@ -1,8 +1,19 @@
 /** @format */
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-import { Avatar, Badge, Row, Col } from 'antd'
+import {
+	Avatar,
+	Badge,
+	Row,
+	Col,
+	Drawer,
+	Form,
+	Button,
+	Input,
+	Select,
+} from 'antd'
 import {
 	NotificationOutlined,
 	PlusOutlined,
@@ -11,14 +22,33 @@ import {
 
 import './style.css'
 
+const { Option } = Select
+
 class User extends React.Component {
+	state = { visible: false }
+
+	showDrawer = () => {
+		this.setState({
+			visible: true,
+		})
+	}
+
+	onClose = () => {
+		this.setState({
+			visible: false,
+		})
+	}
+
 	render() {
 		return (
 			<React.Fragment>
 				<div className='cv-navbar-user-content'>
 					<Row align='middle'>
 						<Col xs={8} sm={8} md={8}>
-							<PlusOutlined style={{ fontSize: '20px' }} />
+							<PlusOutlined
+								style={{ fontSize: '20px' }}
+								onClick={this.showDrawer}
+							/>
 						</Col>
 						<Col xs={8} sm={8} md={8}>
 							<Badge count={5}>
@@ -26,10 +56,139 @@ class User extends React.Component {
 							</Badge>
 						</Col>
 						<Col xs={8} sm={8} md={8}>
-							<Avatar size={28} icon={<UserOutlined />} />
+							<Link to={`/profile`}>
+								<Avatar size={28} icon={<UserOutlined />} />
+							</Link>
 						</Col>
 					</Row>
 				</div>
+				<Drawer
+					title='Crear Nueva cuenta'
+					width={720}
+					onClose={this.onClose}
+					visible={this.state.visible}
+					bodyStyle={{ paddingBottom: 80 }}
+					footer={
+						<div
+							style={{
+								textAlign: 'right',
+							}}>
+							<Button onClick={this.onClose} style={{ marginRight: 8 }}>
+								Cancel
+							</Button>
+							<Button onClick={this.onClose} type='primary'>
+								Crear
+							</Button>
+						</div>
+					}>
+					<Form layout='vertical' hideRequiredMark>
+						<Row gutter={16}>
+							<Col span={24}>
+								<Form.Item name='name' label='Email'>
+									<Input placeholder='Ingrese su correo electronico' />
+								</Form.Item>
+							</Col>
+							<Col span={12}>
+								<Form.Item
+									name='name'
+									label='Nombre'
+									rules={[
+										{ required: true, message: 'Please enter user name' },
+									]}>
+									<Input placeholder='Nombre de la cuenta' />
+								</Form.Item>
+							</Col>
+							<Col span={12}>
+								<Form.Item
+									name='url'
+									label='Imagen'
+									rules={[
+										{ required: true, message: 'Ingresar url de imagen' },
+									]}>
+									<Input
+										style={{ width: '100%' }}
+										placeholder='Ingresar url de imagen'
+									/>
+								</Form.Item>
+							</Col>
+						</Row>
+						<Row gutter={16}>
+							<Col span={12}>
+								<Form.Item
+									name='owner'
+									label='Categoria'
+									rules={[
+										{ required: true, message: 'Please select an owner' },
+									]}>
+									<Select placeholder='Please select an owner'>
+										<Option value='xiao'>Xiaoxiao Fu</Option>
+										<Option value='mao'>Maomao Zhou</Option>
+									</Select>
+								</Form.Item>
+							</Col>
+							<Col span={12}>
+								<Form.Item
+									name='type'
+									label='Tipo de Cuenta'
+									rules={[
+										{ required: true, message: 'Please choose the type' },
+									]}>
+									<Select placeholder='Please choose the type'>
+										<Option value='private'>Private</Option>
+										<Option value='public'>Public</Option>
+									</Select>
+								</Form.Item>
+							</Col>
+						</Row>
+						<Row gutter={16}>
+							<Col span={12}>
+								<Form.Item
+									name='approver'
+									label='País'
+									rules={[
+										{ required: true, message: 'Please choose the approver' },
+									]}>
+									<Select placeholder='Please choose the approver'>
+										<Option value='jack'>Jack Ma</Option>
+										<Option value='tom'>Tom Liu</Option>
+									</Select>
+								</Form.Item>
+							</Col>
+							<Col span={12}>
+								<Form.Item name='dateTime' label='Número de telefono'>
+									<Input.Group>
+										<Row gutter={8}>
+											<Col span={5}>
+												<Input defaultValue='0571' />
+											</Col>
+											<Col span={8}>
+												<Input defaultValue='26888888' />
+											</Col>
+										</Row>
+									</Input.Group>
+								</Form.Item>
+							</Col>
+						</Row>
+						<Row gutter={16}>
+							<Col span={24}>
+								<Form.Item
+									name='description'
+									label='Descripcion'
+									rules={[
+										{
+											required: true,
+											message: 'please enter url description',
+										},
+									]}>
+									<Input.TextArea
+										rows={4}
+										placeholder='please enter url description'
+									/>
+								</Form.Item>
+							</Col>
+						</Row>
+					</Form>
+				</Drawer>
 			</React.Fragment>
 		)
 	}
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -2,7 +2,21 @@
 
 import React from 'react'
 
+import { Layout, Row, Col, Card, Avatar, Space, Typography } from 'antd'
+import {
+	UserOutlined,
+	MailOutlined,
+	UserAddOutlined,
+	FileTextOutlined,
+	PhoneOutlined,
+	ClockCircleOutlined,
+	HeartOutlined,
+} from '@ant-design/icons'
+
 import './style.css'
+
+const { Content, Header } = Layout
+const { Text } = Typography
 
 class Profile extends React.Component {
 	constructor(props) {
@@ -12,9 +26,226 @@ class Profile extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<h1>Profile</h1>
-			</div>
+			<>
+				<Layout>
+					<Content>
+						<Header className='cv-perfil-title-main-container'>
+							<UserOutlined className='cv-perfil-title-main-icon' />
+							<h3 className='cv-perfil-title-main-title'>Perfil de Usuario</h3>
+						</Header>
+					</Content>
+					<Layout className='cv-perfil-main-container'>
+						<Row>
+							<Col span={24}>
+								<Card className='cv-profile-main-container'>
+									<Layout className='cv-profile-container'>
+										<Row>
+											<Col xs={24} sm={10} md={10} lg={14} xl={14}>
+												<Row className='cv-profile-main-info-container'>
+													<Col
+														xs={4}
+														sm={4}
+														md={4}
+														lg={4}
+														xl={4}
+														className='cv-profile-main-info-inner-container'>
+														<Avatar
+															className='cv-profile-avatar'
+															icon={<UserOutlined />}
+														/>
+													</Col>
+													<Col
+														xs={20}
+														sm={20}
+														md={20}
+														lg={20}
+														xl={20}
+														className='cv-profile-main-info-inner-container-2'>
+														<Space direction='vertical'>
+															<Text className='cv-profile-main-title-user'>
+																Carlos Espinoza
+															</Text>
+															<Space>
+																<Text className='cv-profile-main-role-user'>
+																	Roles: Cuenta
+																</Text>
+															</Space>
+															<Space>
+																<Text className='cv-profile-role-title'>
+																	Rol QF:
+																</Text>
+																<Text className='cv-profile-role-description'>
+																	Rol 1
+																</Text>
+															</Space>
+															<Space>
+																<Text className='cv-profile-role-title'>
+																	Rol Tracking:
+																</Text>
+																<Text className='cv-profile-role-description'>
+																	Rol 2
+																</Text>
+															</Space>
+														</Space>
+													</Col>
+												</Row>
+											</Col>
+											<Col
+												xs={24}
+												sm={14}
+												md={14}
+												lg={10}
+												xl={10}
+												className='cv-profile-info-container'>
+												<Space direction='vertical'>
+													<Space direction='horizontal'>
+														<MailOutlined className='cv-profile-info-icon' />
+														<Text className='cv-profile-info-title'>
+															Nombre:{' '}
+														</Text>
+														<Text className='cv-profile-info-description'>
+															Carlos
+														</Text>
+													</Space>
+													<Space direction='horizontal'>
+														<UserOutlined className='cv-profile-info-icon' />
+														<Text className='cv-profile-info-title'>
+															Apellido:{' '}
+														</Text>
+														<Text className='cv-profile-info-description'>
+															Espinoza
+														</Text>
+													</Space>
+													<Space direction='horizontal'>
+														<UserAddOutlined className='cv-profile-info-icon' />
+														<Text className='cv-profile-info-title'>
+															Correo:{' '}
+														</Text>
+														<Text className='cv-profile-info-description'>
+															carlos@gmail.com
+														</Text>
+													</Space>
+													<Space direction='horizontal'>
+														<FileTextOutlined className='cv-profile-info-icon' />
+														<Text className='cv-profile-info-title'>Rut: </Text>
+														<Text className='cv-profile-info-description'>
+															27.2155.134-4
+														</Text>
+													</Space>
+													<Space direction='horizontal'>
+														<PhoneOutlined className='cv-profile-info-icon' />
+														<Text className='cv-profile-info-title'>
+															{' '}
+															Teléfono:{' '}
+														</Text>
+														<Text className='cv-profile-info-description'>
+															+56738473734
+														</Text>
+													</Space>
+													<Space direction='horizontal'>
+														<ClockCircleOutlined className='cv-profile-info-icon' />
+														<Text className='cv-profile-info-title'>
+															Fecha:{' '}
+														</Text>
+														<Text className='cv-profile-info-description'>
+															12/45/544
+														</Text>
+													</Space>
+												</Space>
+											</Col>
+										</Row>
+									</Layout>
+								</Card>
+							</Col>
+
+							<Header className='cv-perfil-title-main-container'>
+								<HeartOutlined className='cv-perfil-title-main-icon' />
+								<h3 className='cv-perfil-title-main-title'>
+									Cuentas Asociadas
+								</h3>
+							</Header>
+
+							<Col span={24}>
+								<Row>
+									<Col xs={24} sm={24} md={16} lg={16} xl={16}>
+										<Card className='cv-perfil-description-container'>
+											<Row>
+												<Col
+													xs={6}
+													sm={6}
+													md={6}
+													lg={6}
+													xl={6}
+													className='cv-perfil-account-img-container'>
+													<img
+														width='100%'
+														title='Cuenta'
+														alt='Cuenta'
+														src='http://pluto.pinsupreme.com/wp-content/uploads/2014/05/AdobeStock_103332683-863x576.jpg'
+													/>
+												</Col>
+												<Col xs={18} sm={18} md={18} lg={18} xl={18}>
+													<h3 className='cv-perfil-description-title'>
+														Lorem Ipsum
+													</h3>
+													<p className='cv-perfil-description'>
+														Lorem ipsum dolor sit amet, consectetur adipiscing
+														elit. Duis sagittis neque nec leo cvaretra porta.
+														Duis posuere tincidunt justo, finibus interdum nunc
+														ultrices non. Cras consectetur facilisis nulla,
+														ultricies cvaretra ante tristique eleifend. Sed
+														finibus leo eu arcu auctor tincidunt. Integer
+														sagittis, elit vitae volutpat dignissim, arcu sapien
+														vehicula ex, non bibendum ante lorem in tortor.
+													</p>
+												</Col>
+											</Row>
+										</Card>
+									</Col>
+									<Col
+										className='cv-perfil-planes-container'
+										xs={24}
+										sm={24}
+										md={8}
+										lg={8}
+										xl={8}>
+										<Card>
+											<div className='cv-perfil-plans-content-plan'>
+												<img
+													className='cv-perfil-plans-images-plan'
+													title='Plan'
+													alt='Plan'
+													src='http://pluto.pinsupreme.com/wp-content/uploads/2014/05/AdobeStock_103332683-100x100.jpg'
+												/>
+												<h3 className='cv-perfil-plans-title-plan-title'>
+													Descripcion
+												</h3>
+												<h4 className='cv-perfil-plans-title-plan-title-price'>
+													Precio: 10.00
+												</h4>
+											</div>
+											<div className='cv-perfil-plans-content-plan'>
+												<img
+													className='cv-perfil-plans-images-plan'
+													title='Plan'
+													alt='Plan'
+													src='http://pluto.pinsupreme.com/wp-content/uploads/2014/05/AdobeStock_103332683-100x100.jpg'
+												/>
+												<h3 className='cv-perfil-plans-title-plan-title'>
+													Descripcion
+												</h3>
+												<h4 className='cv-perfil-plans-title-plan-title-price'>
+													Precio: 10.00
+												</h4>
+											</div>
+										</Card>
+									</Col>
+								</Row>
+							</Col>
+						</Row>
+					</Layout>
+				</Layout>
+			</>
 		)
 	}
 }
