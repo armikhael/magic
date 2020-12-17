40c2b6ab57026787a06770bad7189e7f3452eaa9
Diego Carciente <diego.carciente@gmail.com>
2020-12-17 19:03:08 -0300

    fix

--- src/pages/profile/create-account/services.js
+++ src/pages/profile/create-account/services.js
@@ -35,26 +35,26 @@ const serviceSaveAccount = async (body) => {
 
 const serviceGetInstagramAccount = async (param) => {
 	let returnResponse
-	await axios({
-		method: 'GET',
-		url: `https://www.instagram.com/${param}`,
-	})
-	.then((response) => {
-		const jsonObject = response.data
-		.match(
-			/<script type="text\/javascript">window\._sharedData = (.*)<\/script>/
-		)[1]
-		.slice(0, -1)
-		console.log('error de JSON parse');
-		if (jsonObject) {
-			const jsonParse = JSON.parse(jsonObject)
-			const userInfo = jsonParse.entry_data.ProfilePage[0].graphql.user
-			returnResponse = userInfo
-		}
-	})
-	.catch((error) => {
-		returnResponse = error
-	})
+	// await axios({
+	// 	method: 'GET',
+	// 	url: `https://www.instagram.com/${param}`,
+	// })
+	// .then((response) => {
+	// 	const jsonObject = response.data
+	// 	.match(
+	// 		/<script type="text\/javascript">window\._sharedData = (.*)<\/script>/
+	// 	)[1]
+	// 	.slice(0, -1)
+	// 	console.log('error de JSON parse');
+	// 	if (jsonObject) {
+	// 		const jsonParse = JSON.parse(jsonObject)
+	// 		const userInfo = jsonParse.entry_data.ProfilePage[0].graphql.user
+	// 		returnResponse = userInfo
+	// 	}
+	// })
+	// .catch((error) => {
+	// 	returnResponse = error
+	// })
 	return returnResponse
 }
 
