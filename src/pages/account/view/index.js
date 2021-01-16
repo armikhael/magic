7826102256364f86f301f056ab19f351cda6918d
Diego Carciente <diego.carciente@gmail.com>
2021-01-16 06:29:20 -0300

    feature: cambiando titulo

--- src/pages/account/view/index.js
+++ src/pages/account/view/index.js
@@ -27,7 +27,7 @@ export default class View extends React.Component {
 				<Content className='cv-container-main'>
 					<div className='cv-category-content-title'>
 						<h1 className='cv-category-title'>
-							Resultado de Busqueda: {atob(this.props.match.params.name)}
+							Cuentas asociadas de: {atob(this.props.match.params.name)}
 						</h1>
             <ListMasonry listMasonry={this.state.list} />
 					</div>
