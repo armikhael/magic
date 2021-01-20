5db8eb246d5dedc3e84ffa21e55643bace387d56
Diego Carciente <diego.carciente@gmail.com>
2021-01-20 00:38:51 -0300

    google analitycs

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,17 @@
 /** @format */
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { withRouter } from 'react-router-dom'
-
+import ReactGA from 'react-ga';
 import Header from '../../components/Header/'
 
 function Layout(props) {
+
+	useEffect(() => {
+			ReactGA.initialize('G-V1CD33BQQ4');
+			ReactGA.pageview(window.location.pathname + window.location.search);
+	}, [])
+
 	const handleLayouts = () => {
 		if (props.location.pathname === '/auth/login') {
 			return <>{props.children}</>
