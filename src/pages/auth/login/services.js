25e9671e9e74509a31a296f58fad7b641614ce98
Carlos Espinoza <carlosarmikhael@gmail.com>
2021-01-27 00:21:49 -0300

    services

--- src/pages/auth/login/services.js
+++ src/pages/auth/login/services.js
@@ -1,5 +1,4 @@
 /** @format */
-/** @format */
 
 import axios from 'axios'
 import { notification } from 'antd'
@@ -8,18 +7,18 @@ export default async function serviceSaveUser(item) {
 	let returnResponse
 	await axios({
 		method: 'POST',
-        url: `${process.env.REACT_APP_HOST}/auth/`,
-        data: item
+		url: `${process.env.REACT_APP_HOST}/auth/`,
+		data: item,
 	})
-	.then((response) => {
-		returnResponse = response.data
-		notification['success']({
-			message: `Bienvenido!!`,
-			description: `Su cuenta esta autorizada`,
+		.then((response) => {
+			returnResponse = response.data
+			notification['success']({
+				message: `Bienvenido!!`,
+				description: `Su cuenta esta autorizada`,
+			})
 		})
-	})
-	.catch((error) => {
-		returnResponse = error.response.data
-	})
+		.catch((error) => {
+			returnResponse = error.response.data
+		})
 	return returnResponse
 }
