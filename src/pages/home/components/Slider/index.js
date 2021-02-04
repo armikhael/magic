9e12fdbe47b0f539a55ece899beeebd749c5f671
Diego Carciente <diego.carciente@gmail.com>
2021-02-04 08:37:08 -0300

    fix: cabiando orden del banner

--- src/pages/home/components/Slider/index.js
+++ src/pages/home/components/Slider/index.js
@@ -11,21 +11,21 @@ export default class Slider extends React.Component {
 	render() {
 		return (
 			<>
-				<Carousel className='cv-home-carousel-content' autoplay>
-					<div>
-						<Link to={`/profile/create-account`}>
-							<img
-								width='100%'
-								src='https://i.postimg.cc/fTgDSzC7/banner-cv-influencer.jpg'
-								alt='Banner Publicitario'
-							/>
-						</Link>
-					</div>
+				<Carousel className='cv-home-carousel-content' autoplay>					
 					<div>
 						<Link to={`/help`}>
 							<img
 								width='100%'
 								src='https://i.postimg.cc/ty3J2F7P/banner-cv-general.jpg'
+								alt='Banner Publicitario'
+							/>
+						</Link>
+					</div>
+					<div>
+						<Link to={`/profile/create-account`}>
+							<img
+								width='100%'
+								src='https://i.postimg.cc/fTgDSzC7/banner-cv-influencer.jpg'
 								alt='Banner Publicitario'
 							/>
 						</Link>
