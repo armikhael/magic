7da4dacccfb87e499ee76ac696340ab83031daec
Carlos Espinoza <carlosarmikhael@gmail.com>
2021-02-07 17:00:31 -0300

    slider mobil

--- src/pages/home/components/Slider/index.js
+++ src/pages/home/components/Slider/index.js
@@ -11,7 +11,7 @@ export default class Slider extends React.Component {
 	render() {
 		return (
 			<>
-				<Carousel className='cv-home-carousel-content' autoplay>					
+				<Carousel className='cv-home-carousel-content cv-home-slider-web ' autoplay>
 					<div>
 						<Link to={`/help`}>
 							<img
@@ -26,6 +26,26 @@ export default class Slider extends React.Component {
 							<img
 								width='100%'
 								src='https://i.postimg.cc/fTgDSzC7/banner-cv-influencer.jpg'
+								alt='Banner Publicitario'
+							/>
+						</Link>
+					</div>
+				</Carousel>
+				<Carousel className='cv-home-carousel-content cv-home-slider-mobil' autoplay>
+					<div>
+						<Link to={`/help`}>
+							<img
+								width='100%'
+								src='https://i.postimg.cc/bwz8f8tc/banner-general-mobile.jpg'
+								alt='Banner Publicitario'
+							/>
+						</Link>
+					</div>
+					<div>
+						<Link to={`/profile/create-account`}>
+							<img
+								width='100%'
+								src='https://i.postimg.cc/tCdyc77N/banner-influencer-mobile.jpg'
 								alt='Banner Publicitario'
 							/>
 						</Link>
