45c3fc2ef13f2587c3267ea9bb79f2e3374e36e5
Carlos Espinoza <carlosarmikhael@gmail.com>
2021-02-08 12:13:51 -0300

    modificacion mobil

--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -17,15 +17,16 @@ import { serviceViewAccount } from './services'
 export default class Account extends React.Component {
 	state = {
 		detail: null,
+		relations: null,
 		loading: true,
 	}
 
 	componentDidMount() {
 		serviceViewAccount(this.props.match.params.name).then((response) => {
-			this.setState({ 
-				loading: false, 
+			this.setState({
+				loading: false,
 				detail: response.account[0],
-				relations: response.relations
+				relations: response.relations,
 			})
 		})
 	}
@@ -136,9 +137,10 @@ export default class Account extends React.Component {
 										src='https://www.womgp.com/blog/wp-content/uploads/2018/04/5-tendencias-influencer-marketing-2017-810x473.jpg'
 									/>
 								</div>
-								<a href={`${process.env.REACT_APP_WHATSAPP}?phone=${this.state.detail.code}${this.state.detail.phone}&text=Hola ${this.state.detail.account},+te+encontre+en+cuentasvirales.com+y+quisiera+conversar+sobre+un+intercambio+por+publicidad`}>									
+								<a
+									href={`${process.env.REACT_APP_WHATSAPP}?phone=${this.state.detail.code}${this.state.detail.phone}&text=Hola ${this.state.detail.account},+te+encontre+en+cuentasvirales.com+y+quisiera+conversar+sobre+un+intercambio+por+publicidad`}>
 									<WhatsAppOutlined />
-									&nbsp; Intercambios & Canjes a Convenir									
+									&nbsp; Intercambios & Canjes a Convenir
 								</a>
 								<h3 className='cv-detail-plans-title'>Planes</h3>
 								<div className='cv-detail-plans-hr'></div>
