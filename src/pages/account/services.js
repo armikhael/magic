d7929480eb695e02ce6c5d1e5b8a520e9ab3c07d
Diego Carciente <diego.carciente@gmail.com>
2021-03-18 08:46:43 -0300

    feature: nuevo servicio del detalle de la cuenta

--- src/pages/account/services.js
+++ src/pages/account/services.js
@@ -7,25 +7,26 @@ const serviceViewAccount = async (item) => {
 	let returnResponse
 	await axios({
 		method: 'GET',
-		url: `${process.env.REACT_APP_HOST}/account/view/${item}`,
+		url: `${process.env.REACT_APP_HOST}/account/detail/${item}`,
 	})
-	.then((response) => {
-		if (response.data.statusCode <= 200) {
-			returnResponse = response.data.data
-		} else {
-			notification['error']({
-				message: `Error ${response.data.statusCode}`,
-				description: `Problemas con el servico.`,
-			})
-		}
-	})
-	.catch((error) => {
-		returnResponse = error.response.data
-		notification['error']({
-			message: `Error`,
-			description: `Problemas con el servico process.env.REACT_APP_HOST`,
+		.then((response) => {
+			console.log(response)
+			if (response.data.statusCode <= 200) {
+				returnResponse = response.data.data
+			} else {
+				notification['error']({
+					message: `Error ${response.data.statusCode}`,
+					description: `Problemas con el servico.`,
+				})
+			}
 		})
-	})
+		.catch((error) => {
+			returnResponse = error.response.data
+			notification['error']({
+				message: `Error`,
+				description: `Problemas con el servico process.env.REACT_APP_HOST`,
+			})
+		})
 
 	return returnResponse
 }
