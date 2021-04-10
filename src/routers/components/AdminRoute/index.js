2ed3569e9267207559c36a3501e8f5eb825275f8
Diego Carciente <diego.carciente@gmail.com>
2021-04-10 12:19:20 -0400

    fix: agregando permisos a yas

--- src/routers/components/AdminRoute/index.js
+++ src/routers/components/AdminRoute/index.js
@@ -5,13 +5,14 @@ import { Route, Redirect } from 'react-router-dom'
 
 class AdminRoute extends React.Component {
 	render() {
-    let admin = [
-      'diego.carciente@gmail.com',
-      'carlosarmikhael@gmail.com',
-      'carlos.espinoza@pharol.cl'
-    ]
+		let admin = [
+			'diego.carciente@gmail.com',
+			'carlosarmikhael@gmail.com',
+			'carlos.espinoza@pharol.cl',
+			'yasbarrera2@gmail.com',
+		]
 
-    let isAuthenticated = JSON.parse(localStorage.getItem('user'))
+		let isAuthenticated = JSON.parse(localStorage.getItem('user'))
 		if (admin.includes(isAuthenticated.email)) {
 			return <Route {...this.props} />
 		}
