5b864076f4cbfe9f6e21327184f5c9a662f5ef3f
Diego Carciente <diego.carciente@gmail.com>
2021-04-27 20:12:15 -0400

    integrando google analitycs universal

--- src/components/ServiceCommons/EventsGoogleAnalitycs.js
+++ src/components/ServiceCommons/EventsGoogleAnalitycs.js
@@ -1,6 +1,6 @@
 /** @format */
 import ReactGA from 'react-ga'
-ReactGA.initialize('G-V1CD33BQQ4')
+ReactGA.initialize('UA-187711614-1')
 
 const serviceEventGoogleAnalytics = async (item) => {
 	ReactGA.event({
