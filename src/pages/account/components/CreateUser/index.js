5707de8e8d1d858b2a9d4c31c3b8477a1983fd2c
Carlos Espinoza <carlosarmikhael@gmail.com>
2021-05-11 15:47:31 -0400

    cuantas asociadas

--- src/pages/account/components/CreateUser/index.js
+++ src/pages/account/components/CreateUser/index.js
@@ -8,6 +8,7 @@ import { UserOutlined } from '@ant-design/icons'
 
 export default class CreateUser extends React.Component {
 	render() {
+		console.log(this.props.asociation)
 		return (
 			<>
 				<div className='cv-detail-contente-user-create'>
@@ -20,18 +21,23 @@ export default class CreateUser extends React.Component {
 						{this.props.asociation.length > 0 && (
 							<Col xs={24} sm={24} md={12}>
 								<h3 className='cv-detail-user-create-title'>Cuentas Asociadas</h3>
-								{this.props.asociation.map(function (item, i) {
-									return (
-										<Link key={i} to={`/${item.name}`}>
-											<img
-												width='22px'
-												src={item.interface.icon}
-												alt={item.type}
-												title={item.type}
-											/>
-										</Link>
-									)
-								})}
+								<Row>
+									{this.props.asociation.map(function (item, i) {
+										return (
+											<Col xs={24} sm={24} md={12} key={i}>
+												<Link to={`/${item.name}`}>
+													<img
+														width='22px'
+														src={item.interface.icon}
+														alt={item.type}
+														title={item.type}
+													/>
+													&nbsp;{item.account}
+												</Link>
+											</Col>
+										)
+									})}
+								</Row>
 							</Col>
 						)}
 					</Row>
