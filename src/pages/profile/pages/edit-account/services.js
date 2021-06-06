c1bb9ea48e64e38e3684706e94d297d8757f4e11
Diego Carciente <diego.carciente@gmail.com>
2021-06-06 13:41:13 -0400

    hotfix: actualizacion de los datos de la cuenta

--- src/pages/profile/pages/edit-account/services.js
+++ src/pages/profile/pages/edit-account/services.js
@@ -3,22 +3,19 @@
 import axios from 'axios'
 
 const serviceUpdateAccount = async (body) => {
-  console.log(body);
 	let returnResponse
 	await axios({
 		method: 'PUT',
 		url: `${process.env.REACT_APP_HOST}/account/`,
 		data: body,
 	})
-	.then((response) => {
-		returnResponse = response.data
-	})
-	.catch((e) => {
-		returnResponse = e.response.data
-	})
+		.then((response) => {
+			returnResponse = response.data
+		})
+		.catch((e) => {
+			returnResponse = e.response.data
+		})
 	return returnResponse
 }
-
-
 
 export { serviceUpdateAccount }
