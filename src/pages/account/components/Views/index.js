e68221a497648345c3db380a3179dcb832be724f
Diego Carciente <diego.carciente@gmail.com>
2021-06-15 12:22:47 -0400

    fix: show/hide las funciones de interaccion

--- src/pages/account/components/Views/index.js
+++ src/pages/account/components/Views/index.js
@@ -80,103 +80,111 @@ export default function Views(props) {
 					</Col>
 					<Col xs={24} sm={24} md={12} className='cv-detail-content-actiones-btn'>
 						<p>Intercambios:</p>
-						<Comment
-							author={<p className='cv-detail-actiones-title'>Likes x Likes</p>}
-							avatar={<HeartOutlined style={{ fontSize: '26px' }} />}
-							content={
-								<p>
-									¡Aumenta el alcance de tu cuenta!
-									<br />
-									{isPermission === true && (
-										<Popconfirm
-											title='El influencer dará likes a tus publicaciones y tú a las de el, de esa manera aumentan la interacción en sus cuentas ¿Estás de acuerdo?'
-											okText='Aceptar'
-											cancelText='Rechazar'
-											icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
-											onConfirm={() => {
-												serviceEventGoogleAnalytics({
-													category: 'intercambio',
-													action: 'click-like',
-													label: props.detail.name,
-												})
+						{props.detail.representation === false && (
+							<>
+								<Comment
+									author={<p className='cv-detail-actiones-title'>Likes x Likes</p>}
+									avatar={<HeartOutlined style={{ fontSize: '26px' }} />}
+									content={
+										<p>
+											¡Aumenta el alcance de tu cuenta!
+											<br />
+											{isPermission === true && (
+												<Popconfirm
+													title='El influencer dará likes a tus publicaciones y tú a las de el, de esa manera aumentan la interacción en sus cuentas ¿Estás de acuerdo?'
+													okText='Aceptar'
+													cancelText='Rechazar'
+													icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
+													onConfirm={() => {
+														serviceEventGoogleAnalytics({
+															category: 'intercambio',
+															action: 'click-like',
+															label: props.detail.name,
+														})
 
-												window.open(
-													`${process.env.REACT_APP_WHATSAPP}?phone=${props.detail.code}${props.detail.phone}&text=Hola ${props.detail.account}, te encontre en cuentasvirales.com y me gustaría que hagamos la dinñamica de (LIKE x LIKE)`
-												)
-											}}>
-											<span className='cv-detail-actiones-title-a' style={{ cursor: 'pointer' }}>
-												Click aquí
-											</span>
-										</Popconfirm>
-									)}
-									{isPermission === false && handlePermissions()}
-								</p>
-							}
-						/>
+														window.open(
+															`${process.env.REACT_APP_WHATSAPP}?phone=${props.detail.code}${props.detail.phone}&text=Hola ${props.detail.account}, te encontre en cuentasvirales.com y me gustaría que hagamos la dinñamica de (LIKE x LIKE)`
+														)
+													}}>
+													<span
+														className='cv-detail-actiones-title-a'
+														style={{ cursor: 'pointer' }}>
+														Click aquí
+													</span>
+												</Popconfirm>
+											)}
+											{isPermission === false && handlePermissions()}
+										</p>
+									}
+								/>
 
-						<Comment
-							author={<p className='cv-detail-actiones-title'>Mención x Mención</p>}
-							avatar={<NotificationOutlined style={{ fontSize: '26px' }} />}
-							content={
-								<p>
-									¡Aumenta los seguidores en tu cuenta!
-									<br />
-									{isPermission === true && (
-										<Popconfirm
-											title='El influencer hará una mención en su cuenta, tú en la tuya (a esto le llamamos intercambio publicitario) y de esta manera intercambian seguidores (cada influencer tiene sus propias normas) ¿Estás de acuerdo?'
-											okText='Aceptar'
-											cancelText='Rechazar'
-											icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
-											onConfirm={() => {
-												serviceEventGoogleAnalytics({
-													category: 'intercambio',
-													action: 'click-mencion',
-													label: props.detail.name,
-												})
+								<Comment
+									author={<p className='cv-detail-actiones-title'>Mención x Mención</p>}
+									avatar={<NotificationOutlined style={{ fontSize: '26px' }} />}
+									content={
+										<p>
+											¡Aumenta los seguidores en tu cuenta!
+											<br />
+											{isPermission === true && (
+												<Popconfirm
+													title='El influencer hará una mención en su cuenta, tú en la tuya (a esto le llamamos intercambio publicitario) y de esta manera intercambian seguidores (cada influencer tiene sus propias normas) ¿Estás de acuerdo?'
+													okText='Aceptar'
+													cancelText='Rechazar'
+													icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
+													onConfirm={() => {
+														serviceEventGoogleAnalytics({
+															category: 'intercambio',
+															action: 'click-mencion',
+															label: props.detail.name,
+														})
 
-												window.open(
-													`${process.env.REACT_APP_WHATSAPP}?phone=${props.detail.code}${props.detail.phone}&text=Hola ${props.detail.account}, te encontre en cuentasvirales.com y me gustaría que hagamos la dinámica de (MENCIÓN x MENCIÓN)`
-												)
-											}}>
-											<span className='cv-detail-actiones-title-a' style={{ cursor: 'pointer' }}>
-												Click aquí
-											</span>
-										</Popconfirm>
-									)}
-									{isPermission === false && handlePermissions()}
-								</p>
-							}
-						/>
+														window.open(
+															`${process.env.REACT_APP_WHATSAPP}?phone=${props.detail.code}${props.detail.phone}&text=Hola ${props.detail.account}, te encontre en cuentasvirales.com y me gustaría que hagamos la dinámica de (MENCIÓN x MENCIÓN)`
+														)
+													}}>
+													<span
+														className='cv-detail-actiones-title-a'
+														style={{ cursor: 'pointer' }}>
+														Click aquí
+													</span>
+												</Popconfirm>
+											)}
+											{isPermission === false && handlePermissions()}
+										</p>
+									}
+								/>
 
-						<Comment
-							author={<p className='cv-detail-actiones-title'>Producto x Mención</p>}
-							avatar={<AuditOutlined style={{ fontSize: '26px' }} />}
-							content={
-								<p>
-									¡Aumenta tu credibilidad!
-									<br />
-									<Popconfirm
-										title='El influencer te pedirá un "PRODUCTO" a cambio de la publicidad, el influencer se quedará con dicho producto que primero debe probar y hará la mención de tu negocio (cada influencer tiene sus propias normas) ¿Estás de acuerdo?'
-										okText='Si'
-										cancelText='No'
-										icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
-										onConfirm={() => {
-											serviceEventGoogleAnalytics({
-												category: 'intercambio',
-												action: 'click-producto',
-												label: props.detail.name,
-											})
-											window.open(
-												`${process.env.REACT_APP_WHATSAPP}?phone=${props.detail.code}${props.detail.phone}&text=Hola ${props.detail.account}, te encontre en cuentasvirales.com y me gustaría darte un PRODUCTO por una mención en tu cuenta`
-											)
-										}}>
-										<a href={'/#'} className='cv-detail-actiones-title-a'>
-											Click aquí
-										</a>
-									</Popconfirm>
-								</p>
-							}
-						/>
+								<Comment
+									author={<p className='cv-detail-actiones-title'>Producto x Mención</p>}
+									avatar={<AuditOutlined style={{ fontSize: '26px' }} />}
+									content={
+										<p>
+											¡Aumenta tu credibilidad!
+											<br />
+											<Popconfirm
+												title='El influencer te pedirá un "PRODUCTO" a cambio de la publicidad, el influencer se quedará con dicho producto que primero debe probar y hará la mención de tu negocio (cada influencer tiene sus propias normas) ¿Estás de acuerdo?'
+												okText='Si'
+												cancelText='No'
+												icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
+												onConfirm={() => {
+													serviceEventGoogleAnalytics({
+														category: 'intercambio',
+														action: 'click-producto',
+														label: props.detail.name,
+													})
+													window.open(
+														`${process.env.REACT_APP_WHATSAPP}?phone=${props.detail.code}${props.detail.phone}&text=Hola ${props.detail.account}, te encontre en cuentasvirales.com y me gustaría darte un PRODUCTO por una mención en tu cuenta`
+													)
+												}}>
+												<a href={'/#'} className='cv-detail-actiones-title-a'>
+													Click aquí
+												</a>
+											</Popconfirm>
+										</p>
+									}
+								/>
+							</>
+						)}
 					</Col>
 				</Row>
 			</div>
