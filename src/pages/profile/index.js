5e585b0fd75608a82a13b82e52329c300839879f
Diego Carciente <diego.carciente@gmail.com>
2021-06-17 14:08:52 -0400

    fix: optimizando componente del usuario en el perfil

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -2,18 +2,17 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
-import { Layout, Row, Col, Card, Result, Typography, Button, notification, Form, Tag } from 'antd'
-import { UserOutlined, HeartOutlined, ApiOutlined, CloseOutlined, CopyOutlined } from '@ant-design/icons'
+import { Layout, Row, Col, Card, Result, Typography, Button, notification, Form } from 'antd'
+import { UserOutlined, HeartOutlined, ApiOutlined } from '@ant-design/icons'
 
 import Loading from '../../components/Loading/Loading'
 import InputField from '../../components/Input'
 
 import './style.css'
 import { serviceGetAccountsByEmail, serviceDeleteAccount, serviceChangePassword } from './services'
-import ModalConfiguration from './components/ModalConfiguration'
-import ModalEdit from './components/ModalEdit'
 import LinkTree from './components/LinkTree'
+import Accounts from './components/Accounts'
+
 import serviceDeleteLinktree from './components/LinkTree/service'
 
 const { Content, Header } = Layout
@@ -122,107 +121,11 @@ export default class Profile extends React.Component {
 								<Row>
 									<Col xs={24} sm={24} md={24} lg={24} xl={24}>
 										<Col xs={24} sm={24} md={24} lg={24} xl={24}>
-											{this.state.accounts.map((item, i) => {
-												return (
-													<Row className='cv-profile-card-account-content' key={i}>
-														<Col sm={24} md={6} className='cv-profile-upload-image'>
-															<img
-																className='cv-profile-main-info-inner-container-img'
-																src={item.image || process.env.REACT_APP_LOGO}
-																alt={item.name}
-																title={item.name}
-															/>
-														</Col>
-														<Col
-															sm={24}
-															md={18}
-															className='cv-profile-account-detail-acount'>
-															<Row>
-																<Col span={24}>
-																	<h3 className='cv-profile-account-detail-title'>
-																		{item.account}
-																	</h3>
-																</Col>
-																<Col span={24} className='mt15'>
-																	{item.emailAccount}
-																	<p>{item.biography}</p>
-																</Col>
-															</Row>
-															<Row>
-																<Col
-																	className='mb15'
-																	xs={24}
-																	sm={24}
-																	md={24}
-																	lg={5}
-																	xl={5}>
-																	<Tag
-																		color='#EC428D'
-																		style={{ textTransform: 'capitalize' }}>
-																		{item.type}
-																	</Tag>
-																</Col>
-																<Col
-																	className='mb15'
-																	xs={24}
-																	sm={24}
-																	md={24}
-																	lg={24}
-																	xl={24}>
-																	{item.eneable !== true && (
-																		<Button
-																			style={{ margin: '0px 5px' }}
-																			shape='round'
-																			href={`/profile/account-activation/${item.name}/modify`}>
-																			Activar Cuenta
-																		</Button>
-																	)}
+											<Accounts
+												componentData={this.state.accounts}
+												componentDelete={this.handleDeleteAccount}
+											/>
 
-																	<Button
-																		style={{ margin: '0px 5px' }}
-																		type='danger'
-																		shape='circle'
-																		onClick={() => {
-																			this.handleDeleteAccount(item)
-																		}}>
-																		<CloseOutlined />
-																	</Button>
-
-																	{item.eneable === true && (
-																		<>
-																			<ModalEdit
-																				componentData={item}
-																				componentHeader={
-																					'Modificar Información'
-																				}
-																			/>
-																			<ModalConfiguration
-																				componentData={item}
-																				componentHeader={'Condiguración'}
-																			/>
-																			<CopyToClipboard
-																				text={`${process.env.REACT_APP_DOMAIN}/${item.name}`}>
-																				<Button
-																					style={{ margin: '0px 5px' }}
-																					shape='circle'
-																					onClick={() => {
-																						notification['success']({
-																							message: '¡Excelente!',
-																							description: `Enlace Copiado.`,
-																							key: i,
-																						})
-																					}}>
-																					<CopyOutlined />
-																				</Button>
-																			</CopyToClipboard>
-																		</>
-																	)}
-																</Col>
-															</Row>
-														</Col>
-													</Row>
-												)
-											})}
 											{(() => {
 												if (this.state.accounts.length === 0) {
 													return (
