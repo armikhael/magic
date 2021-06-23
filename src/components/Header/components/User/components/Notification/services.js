88a4da7dd1cc06e9d8f83c003ad2f3c3d99139ae
Diego Carciente <diego.carciente@gmail.com>
2021-06-23 09:57:32 -0400

    fix: filtro por fecha

--- src/components/Header/components/User/components/Notification/services.js
+++ src/components/Header/components/User/components/Notification/services.js
@@ -3,13 +3,17 @@
 import axios from 'axios'
 
 const serviceGetData = async () => {
+	const date = new Date()
+	console.log(date.getMonth(), date.getDate())
 	let returnResponse
 	await axios({
 		method: 'GET',
 		url: `${process.env.REACT_APP_HOST}/promotion/`,
-		data: {
+		params: {
 			query: {
 				type_promotion: 'account',
+				month: { $gte: date.getMonth() + 1 },
+				day: { $gte: date.getDate() },
 			},
 			sort: {},
 			page: 1,
