96e9f5f5bfd58e39a5b3ec8a60025cbb05a3ac99
Diego Carciente <diego.carciente@gmail.com>
2021-06-24 12:08:44 -0400

    fix: resultado de las notificaciones

--- src/components/Header/components/User/components/Notification/index.js
+++ src/components/Header/components/User/components/Notification/index.js
@@ -15,28 +15,11 @@ export default function Notification() {
 
 	useEffect(() => {
 		serviceGetData().then((response) => {
-			setData([response.data.data[0]])
-			handleSetInterval(response.data.data)
+			const suffle = lodash.shuffle(response.data.data)
+			setData(suffle)
 		})
 		console.log('useEffect')
 	}, []) // eslint-disable-line react-hooks/exhaustive-deps
-
-	const handleSetInterval = async (item) => {
-		item = lodash.shuffle(item)
-		let newArray = []
-		for (var i = 1; i < item.length; i++) {
-			console.log(item[i])
-			item[i].key = i
-			newArray.push(item[i])
-			let orderBy = lodash.orderBy(newArray, ['key'], ['desc'])
-			setData([...orderBy])
-			await sleep(5000)
-		}
-	}
-
-	const sleep = (item) => {
-		return new Promise((resolve) => setTimeout(resolve, item))
-	}
 
 	const menu = data.map((item, key) => {
 		return (
