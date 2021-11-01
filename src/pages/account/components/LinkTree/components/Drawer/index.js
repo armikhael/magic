ebb343f2f72611438bdb521d2a6e7163ed0b1b44
Carlos Espinoza <carlosarmikhael@gmail.com>
2021-11-01 19:47:31 -0300

    publicidad en detalle de cuentas de empresa

--- src/pages/account/components/LinkTree/components/Drawer/index.js
+++ src/pages/account/components/LinkTree/components/Drawer/index.js
@@ -2,35 +2,41 @@
 
 import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import { Button, Drawer, Row, Col } from 'antd'
-import { BellOutlined, RightCircleOutlined } from '@ant-design/icons'
+
+import { Button, Drawer, Row, Col, Layout } from 'antd'
+import { BellOutlined } from '@ant-design/icons'
 
 import serviceEventGoogleAnalytics from '../../../../../../components/ServiceCommons/EventsGoogleAnalitycs'
 
 import './style.css'
 
-const DrawerLinktree = (props) => {
+const DrawerLinktree = () => {
+	const { Content } = Layout
 	const [showDrawer, setShowDrawer] = useState(false)
+	const [isMobil, setMobile] = useState('260')
 	const history = useHistory()
-
-	useEffect(() => {
-		console.log('useEffect')
-	}, [props])
-
 	const data = [
 		{
 			key: '1',
 			text: 'Publicidad en Instagram',
 			description: 'Conoce nuestro servicio de posicionamiento',
 			link: '/pricing',
+			img: 'https://i.ibb.co/0txC9QY/1.jpg',
 		},
 		{
 			key: '2',
 			text: 'Enlace Personalizado',
 			description: 'Registrate como negocio y sigue los pasos',
 			link: '/auth/register',
+			img: 'https://i.ibb.co/QKzTjfR/3.jpg',
 		},
 	]
+
+	useEffect(() => {
+		if (window.innerWidth < 1025) {
+			setMobile('550')
+		}
+	}, [])
 
 	return (
 		<>
@@ -57,70 +63,106 @@ const DrawerLinktree = (props) => {
 					setShowDrawer(false)
 				}}
 				visible={showDrawer}
-				key={'bottom'}>
-				<Row
-					onClick={() => {
-						serviceEventGoogleAnalytics({
-							action: 'click-linktree',
-							category: 'anuncios',
-							label: 'Master Class',
-						})
-						window.open(
-							'https://api.whatsapp.com/send?phone=573106044125&text=Hola,%20vi%20un%20anuncio%20en%20@cuentasvirales%20y%20quisiera%20informaci%C3%B3n%20de%20la%20master%20class'
-						)
-					}}>
-					<Col xs={24} sm={24} md={8}>
-						<Row className='cv-account-linktree-content-card'>
-							<Col xs={3} sm={3} md={3}>
-								<img
-									src='https://i.postimg.cc/YSQXZWCP/logo.jpg'
-									title='Información'
-									alt='Información'
-								/>
-							</Col>
-							<Col xs={17} sm={17} md={17} className='cv-account-linktree-content-card-two'>
-								<h3>Master Class</h3>
-								<p>Mejora el rendimiento de tu cuenta de instagram</p>
-							</Col>
-							<Col xs={4} sm={4} md={4} className='cv-account-linktree-content-card-three'>
-								<RightCircleOutlined style={{ fontSize: '25px', color: '#797979' }} />
-							</Col>
-						</Row>
-					</Col>
-					{data.map((item) => {
-						return (
-							<Col
-								xs={24}
-								sm={24}
-								md={8}
-								onClick={() => {
-									serviceEventGoogleAnalytics({
-										action: 'click-linktree',
-										category: 'anuncios',
-										label: item.text,
-									})
-									history.push(item.link)
-								}}>
-								<Row className='cv-account-linktree-content-card'>
-									<Col xs={3} sm={3} md={3}>
-										<img
-											src='https://i.postimg.cc/YSQXZWCP/logo.jpg'
-											title='Información'
-											alt='Información'
-										/>
-									</Col>
-									<Col xs={17} sm={17} md={17} className='cv-account-linktree-content-card-two'>
-										<h3>{item.text}</h3>
-										<p>{item.description}</p>
-									</Col>
-									<Col xs={4} sm={4} md={4} className='cv-account-linktree-content-card-three'>
-										<RightCircleOutlined style={{ fontSize: '25px', color: '#797979' }} />
-									</Col>
-								</Row>
-							</Col>
-						)
-					})}
-				</Row>
+				key={'bottom'}
+				height={isMobil}>
+				<Content className='cv-container-main'>
+					<div className='cv-account-linktree-drawer-titles-contens'>
+						<br />
+						<h3 className='cv-account-linktree-drawer-title'>
+							Te Ayudamos a{' '}
+							<span className='cv-account-linktree-drawer-title-color'>
+								crecer
+							</span>{' '}
+							en tu cuenta{' '}
+						</h3>
+						<span className='cv-account-linktree-drawer-sub-title'>
+							Ten más seguidores, más interacción, más ventas...
+						</span>
+					</div>
+					<Row
+						onClick={() => {
+							serviceEventGoogleAnalytics({
+								action: 'click-linktree',
+								category: 'anuncios',
+								label: 'Master Class',
+							})
+							window.open(
+								'https://api.whatsapp.com/send?phone=573106044125&text=Hola,%20vi%20un%20anuncio%20en%20@cuentasvirales%20y%20quisiera%20informaci%C3%B3n%20de%20la%20master%20class'
+							)
+						}}>
+						<Col xs={24} sm={24} md={8}>
+							<Row className='cv-account-linktree-content-card'>
+								<Col xs={7} sm={7} md={7}>
+									<img
+										className='cv-account-linktree-content-card-img'
+										width='100%'
+										src='https://i.ibb.co/Ptb6Ldb/2.jpg'
+										title='Información'
+										alt='Información'
+									/>
+								</Col>
+								<Col
+									xs={17}
+									sm={17}
+									md={17}
+									className='cv-account-linktree-content-card-two'>
+									<h3>Master Class</h3>
+									<p>Mejora el rendimiento de tu cuenta de instagram</p>
+									<div>
+										<Button
+											className='cv-account-linktree-drawer-card-button'
+											type='primary'>
+											Click aquí
+										</Button>
+									</div>
+								</Col>
+							</Row>
+						</Col>
+						{data.map((item) => {
+							return (
+								<Col
+									xs={24}
+									sm={24}
+									md={8}
+									onClick={() => {
+										serviceEventGoogleAnalytics({
+											action: 'click-linktree',
+											category: 'anuncios',
+											label: item.text,
+										})
+										history.push(item.link)
+									}}>
+									<Row className='cv-account-linktree-content-card'>
+										<Col xs={7} sm={7} md={7}>
+											<img
+												className='cv-account-linktree-content-card-img'
+												width='100%'
+												src={item.img}
+												title='Información'
+												alt='Información'
+											/>
+										</Col>
+										<Col
+											xs={17}
+											sm={17}
+											md={17}
+											className='cv-account-linktree-content-card-two'>
+											<h3>{item.text}</h3>
+											<p>{item.description}</p>
+											<div>
+												<Button
+													className='cv-account-linktree-drawer-card-button'
+													type='primary'>
+													Click aquí
+												</Button>
+											</div>
+										</Col>
+									</Row>
+								</Col>
+							)
+						})}
+					</Row>
+				</Content>
 			</Drawer>
 		</>
 	)
