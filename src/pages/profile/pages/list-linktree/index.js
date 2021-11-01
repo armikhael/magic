2706376dd18f24b8647258bff67dbf6842abba79
Amilcar Barahona <amilcar.laniakea@gmail.com>
2021-11-01 17:44:57 -0400

    agregada libreria sass, y estilo a la vista de enlaces de negocio

--- src/pages/profile/pages/list-linktree/index.js
+++ src/pages/profile/pages/list-linktree/index.js
@@ -1,88 +1,95 @@
 /** @format */
 
 import React, { useEffect, useState } from 'react'
-import { useHistory } from 'react-router-dom'
-import { Layout, Row, Card, Button, Result, Col } from 'antd'
-import { SisternodeOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+
+import lodash from 'lodash'
+
+import { Row, Result, Col } from 'antd'
+import { SisternodeOutlined, PlusOutlined } from '@ant-design/icons'
 
 import Loading from '../../../../components/Loading/Loading'
 import LinkTree from '../../components/LinkTree'
 
-import { serviceGetData } from './services'
+import { serviceGetData, serviceDelete } from './services'
 
-const { Header } = Layout
+import './style.sass'
 
-const ListLinkTree = (props) => {
-	const history = useHistory()
-	const [data, setData] = useState([])
-	const [loading, setLoading] = useState(true)
-	const [user] = useState(JSON.parse(localStorage.getItem('user')))
+export default function ListLinkTree() {
+	const [isData, setData] = useState(null)
+	const [isUser] = useState(JSON.parse(localStorage.getItem('user')))
+	const [isCheckAcountVerified, setCheckAcountVerified] = useState([])
 
-	const fetchData = async (item) => {
-		const response = await serviceGetData(item.email)
-		console.log(response)
-		setLoading(false)
-		setData(response.links)
+	const handleDeleteLink = async (item) => {
+		serviceDelete(item).then((response) => {
+			if (response) {
+				setData(lodash.orderBy(response, ['eneable'], ['asc']))
+				setCheckAcountVerified(
+					response.filter((item) => {
+						return item.eneable === false
+					})
+				)
+			}
+		})
 	}
 
 	useEffect(() => {
-		fetchData(user)
-		console.log('useEffects')
-	}, [user])
+		serviceGetData(isUser.email).then((response) => {
+			if (response) {
+				setData(lodash.orderBy(response.links, ['eneable'], ['asc']))
+				setCheckAcountVerified(
+					response.links.filter((item) => {
+						return item.eneable === false
+					})
+				)
+			}
+		})
+	}, [isUser])
 
-	if (loading === true) {
+	if (isData) {
+		return (
+			<div className='cv-profile-business-main-container'>
+				<div className='cv-profile-business-head-container'>
+					<h1 className='cv-profile-business-head-title'>{`¡Te damos la bienvenida, ${isUser.first_name} ${isUser.last_name}!`}</h1>
+					<div className='cv-profile-business-spacer'></div>
+					{isData.length > 0 && (
+						<Link className='cv-profile-business-add-button' to='/profile/linktree-name'>
+							<PlusOutlined className='cv-profile-business-add-icon' />
+							Agregar Cuenta
+						</Link>
+					)}
+				</div>
+				<Row>
+					<Col span={24}>
+						<div className='cv-profile-business-title-container'>
+							<SisternodeOutlined className='cv-profile-business-title-icon' />
+							<h3 className='cv-profile-business-title'>Mis Enlaces de Negocio</h3>
+						</div>
+						{isData.length > 0 ? (
+							<LinkTree
+								componentData={isData}
+								checkAcountVerified={isCheckAcountVerified}
+								handleDeleteLink={(data) => handleDeleteLink(data)}
+							/>
+						) : (
+							<Result
+								className='cv-profile-business-no-results-container'
+								status='error'
+								title='Enlaces Registrados'
+								subTitle='No tienes ningún enlace personalizado todavía.'
+								extra={
+									<Link
+										className='cv-profile-business-main-button'
+										to='/profile/linktree-name'>
+										Registrar
+									</Link>
+								}></Result>
+						)}
+					</Col>
+				</Row>
+			</div>
+		)
+	} else {
 		return <Loading />
 	}
-
-	return (
-		<>
-			<div>
-				<Layout className='cv-perfil-main-container'>
-					<Row justify='center'>
-						<Col xs={22} sm={20} xl={10}>
-							<Header className='cv-perfil-title-main-container'>
-								<SisternodeOutlined className='cv-perfil-title-main-icon' />
-								<h3 className='cv-perfil-title-main-title'>Mis Enlaces de Negocio</h3>
-							</Header>
-
-							<Card className='cv-profile-card-account-content'>
-								{data.length > 0 && <LinkTree componentData={data} />}
-
-								{data.length <= 0 && (
-									<Result
-										status='error'
-										title='Enlaces Registrados'
-										subTitle='No tienes ningún enlace personalizado todavía.'
-										extra={[
-											<Button
-												key='button'
-												onClick={() => {
-													history.push(`/profile/linktree-name`)
-												}}
-												className={'cv-account-wizzard-button-submit'}>
-												Registrar
-											</Button>,
-										]}></Result>
-								)}
-							</Card>
-						</Col>
-					</Row>
-
-					{data.length > 0 && (
-						<Row>
-							<Button
-								onClick={() => {
-									history.push(`/profile/linktree-name`)
-								}}
-								className={'cv-account-wizzard-button-submit'}>
-								Agregar
-							</Button>
-						</Row>
-					)}
-				</Layout>
-			</div>
-		</>
-	)
 }
-
-export default ListLinkTree
